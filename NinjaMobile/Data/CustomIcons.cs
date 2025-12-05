78e13a63e1e1b170412eab792a854936d375c453
akchoudhary1980 <73695195+akchoudhary1980@users.noreply.github.com>
2025-12-05 11:18:15 +0530

    Anil Data given

---
+++ NinjaMobile/Data/CustomIcons.cs
@@ -0,0 +1,121 @@
+﻿namespace NinjaMobile.Data
+{
+    public static class CustomIcons
+    {
+        public const string FoodIcon = @"<svg width=""24"" height=""24"" viewBox=""0 0 24 24"" fill=""none"" xmlns=""http://www.w3.org/2000/svg"">
+        <path d=""M14 9.01001L14.01 8.9989"" stroke=""  #666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M8 8.01001L8.01 7.9989"" stroke=""  #666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M8 14.01L8.01 13.9989"" stroke=""  #666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M5.99996 19L2.23622 3.0041C2.13083 2.55618 2.54811 2.16122 2.98957 2.29106L19 7"" stroke=""#FF4035"" stroke-width=""1.5""/>
+        <g filter=""url(#filter0_i_683_14995)"">
+        <path d=""M22.1982 8.42472C22.4326 7.48708 21.8625 6.53695 20.9249 6.30254C19.9872 6.06813 19.0371 6.63821 18.8027 7.57585C18.4108 9.14323 16.9017 11.6242 14.5755 13.9504C12.2745 16.2514 9.4273 18.1443 6.60688 18.7952C5.66513 19.0125 5.07787 19.9521 5.2952 20.8938C5.51252 21.8356 6.45214 22.4228 7.39389 22.2055C11.0736 21.3564 14.4764 18.9992 17.0504 16.4253C19.5991 13.8765 21.59 10.8574 22.1982 8.42472Z"" fill=""#FF3B30""/>
+        </g>
+        <defs>
+        <filter id=""filter0_i_683_14995"" x=""5.25"" y=""6.24988"" width=""17.001"" height=""16.0007"" filterUnits=""userSpaceOnUse"" color-interpolation-filters=""sRGB"">
+        <feFlood flood-opacity=""0"" result=""BackgroundImageFix""/>
+        <feBlend mode=""normal"" in=""SourceGraphic"" in2=""BackgroundImageFix"" result=""shape""/>
+        <feColorMatrix in=""SourceAlpha"" type=""matrix"" values=""0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"" result=""hardAlpha""/>
+        <feOffset/>
+        <feGaussianBlur stdDeviation=""1""/>
+        <feComposite in2=""hardAlpha"" operator=""arithmetic"" k2=""-1"" k3=""1""/>
+        <feColorMatrix type=""matrix"" values=""0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0""/>
+        <feBlend mode=""normal"" in2=""shape"" result=""effect1_innerShadow_683_14995""/>
+        </filter>
+        </defs>
+        </svg>";
+        public const  string VenueIcons = @"<svg width=""24"" height=""24"" viewBox=""0 0 24 24"" fill=""none"" xmlns=""http://www.w3.org/2000/svg"">
+        <path d=""M3 21H21V12C21 9.61305 20.0518 7.32387 18.364 5.63604C16.6761 3.94821 14.3869 3 12 3C9.61305 3 7.32387 3.94821 5.63604 5.63604C3.94821 7.32387 3 9.61305 3 12V21Z"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M3 17H21"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M9 17V13H21"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M13 13V9H20"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        </svg>";
+        public const string EventIcon = @"<svg width=""24"" height=""24"" viewBox=""0 0 24 24"" fill=""none"" xmlns=""http://www.w3.org/2000/svg"">
+        <path d=""M15 4V2M15 4V6M15 4H10.5M3 10V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V10H3Z"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M3 10V6C3 4.89543 3.89543 4 5 4H7"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M7 2V6"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M21 10V6C21 4.89543 20.1046 4 19 4H18.5"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        </svg>";
+        public const string CorporateIcon = @"<svg width=""24"" height=""24"" viewBox=""0 0 24 24"" fill=""none"" xmlns=""http://www.w3.org/2000/svg"">
+        <path d=""M7 9.01001L7.01 8.9989"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M11 9.01001L11.01 8.9989"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M7 13.01L7.01 12.9989"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M11 13.01L11.01 12.9989"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M7 17.01L7.01 16.9989"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M11 17.01L11.01 16.9989"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M15 21H3.6C3.26863 21 3 20.7314 3 20.4V5.6C3 5.26863 3.26863 5 3.6 5H9V3.6C9 3.26863 9.26863 3 9.6 3H14.4C14.7314 3 15 3.26863 15 3.6V9M15 21H20.4C20.7314 21 21 20.7314 21 20.4V9.6C21 9.26863 20.7314 9 20.4 9H15M15 21V17M15 9V13M15 17V13M15 17H17M15 13H17"" stroke=""#666666"" stroke-width=""1.5"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        </svg>";
+
+        public const string Food = @"<svg width=""24"" height=""24"" viewBox=""0 0 24 24"" fill=""none"" xmlns=""http://www.w3.org/2000/svg"">
+        <path d=""M4 11H20C20.2652 11 20.5196 11.1054 20.7071 11.2929C20.8946 11.4804 21 11.7348 21 12V12.5C21 14 18.483 18.073 17 19V20C17 20.2652 16.8946 20.5196 16.7071 20.7071C16.5196 20.8946 16.2652 21 16 21H8C7.73478 21 7.48043 20.8946 7.29289 20.7071C7.10536 20.5196 7 20.2652 7 20V19C5.313 17.946 3 14 3 12.5V12C3 11.7348 3.10536 11.4804 3.29289 11.2929C3.48043 11.1054 3.73478 11 4 11Z"" stroke=""#999999"" stroke-width=""2"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M11.9995 4C11.6831 4.22717 11.4266 4.52797 11.2524 4.87642C11.0782 5.22488 10.9914 5.6105 10.9995 6C10.9914 6.3895 11.0782 6.77512 11.2524 7.12358C11.4266 7.47203 11.6831 7.77283 11.9995 8"" stroke=""#999999"" stroke-width=""2"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M15.9995 4C15.6831 4.22717 15.4266 4.52797 15.2524 4.87642C15.0782 5.22488 14.9914 5.6105 14.9995 6C14.9914 6.3895 15.0782 6.77512 15.2524 7.12358C15.4266 7.47203 15.6831 7.77283 15.9995 8"" stroke=""#999999"" stroke-width=""2"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M7.99955 4C7.68306 4.22717 7.42662 4.52797 7.2524 4.87642C7.07817 5.22488 6.99139 5.6105 6.99955 6C6.99139 6.3895 7.07817 6.77512 7.2524 7.12358C7.42662 7.47203 7.68306 7.77283 7.99955 8"" stroke=""#999999"" stroke-width=""2"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        </svg>";
+
+        public const string Waiter = @"<svg width=""24"" height=""24"" viewBox=""0 0 24 24"" fill=""none"" xmlns=""http://www.w3.org/2000/svg"">
+        <path d=""M11.9996 3C13.9176 3 15.5196 4.35 15.9096 6.151C16.9337 5.87385 18.026 6.01488 18.9461 6.54308C19.8663 7.07128 20.5389 7.94337 20.8161 8.9675C21.0932 9.99163 20.9522 11.0839 20.424 12.0041C19.8958 12.9242 19.0237 13.5968 17.9996 13.874V21H5.99958V13.874C5.49249 13.7366 5.01741 13.5007 4.60147 13.1798C4.18554 12.8588 3.8369 12.4591 3.57546 12.0034C3.31401 11.5477 3.14489 11.0449 3.07774 10.5239C3.01058 10.0028 3.04672 9.4736 3.18408 8.9665C3.32145 8.4594 3.55735 7.98432 3.87831 7.56839C4.19928 7.15246 4.59902 6.80382 5.05473 6.54237C5.51043 6.28093 6.01317 6.1118 6.53423 6.04465C7.0553 5.9775 7.58448 6.01364 8.09158 6.151C8.28542 5.25857 8.7788 4.45933 9.48973 3.88611C10.2007 3.31289 11.0863 3.0002 11.9996 3Z"" stroke=""#999999"" stroke-width=""2"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        <path d=""M6.16113 17.009L18.0001 17"" stroke=""#999999"" stroke-width=""2"" stroke-linecap=""round"" stroke-linejoin=""round""/>
+        </svg>";
+
+        public const string Veg = @"<svg xmlns=""http://www.w3.org/2000/svg"" xml:space=""preserve"" width=""18px"" height=""18px"" version=""1.1"" style=""shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd""
+        viewBox=""0 0 33.77 33.77""
+         xmlns:xlink=""http://www.w3.org/1999/xlink""
+         xmlns:xodm=""http://www.corel.com/coreldraw/odm/2003"">
+         <defs>
+          <style type=""text/css"">
+           <![CDATA[
+            .str0 {stroke:#48AC3C;stroke-width:3.5;stroke-miterlimit:22.9256}
+            .fil0 {fill:#FEFEFE}
+            .fil1 {fill:#48AC3C}
+           ]]>
+          </style>
+         </defs>
+         <g id=""Layer_x0020_1"">
+          <metadata id=""CorelCorpID_0Corel-Layer""/>
+          <g id=""_2470439860656"">
+           <rect class=""fil0 str0"" x=""1.75"" y=""1.75"" width=""30.26"" height=""30.26""/>
+           <circle class=""fil1"" cx=""16.88"" cy=""16.88"" r=""9.94""/>
+          </g>
+         </g>
+        </svg>";
+        public const string NonVeg = @"<svg xmlns=""http://www.w3.org/2000/svg"" xml:space=""preserve"" width=""18px"" height=""18px"" version=""1.1"" style=""shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd""
+        viewBox=""0 0 5.83 5.83""
+         xmlns:xlink=""http://www.w3.org/1999/xlink""
+         xmlns:xodm=""http://www.corel.com/coreldraw/odm/2003"">
+         <defs>
+          <style type=""text/css"">
+           <![CDATA[
+            .str0 {stroke:#E31E24;stroke-width:0.6;stroke-miterlimit:22.9256}
+            .fil0 {fill:#FEFEFE}
+            .fil1 {fill:#E31E24}
+           ]]>
+          </style>
+         </defs>
+         <g id=""Layer_x0020_1"">
+          <metadata id=""CorelCorpID_0Corel-Layer""/>
+          <g id=""_2470446514544"">
+           <rect class=""fil0 str0"" x=""0.3"" y=""0.3"" width=""5.22"" height=""5.22""/>
+           <circle class=""fil1"" cx=""2.91"" cy=""2.91"" r=""1.71""/>
+          </g>
+         </g>
+        </svg>";
+
+        public const string Circle = @"<svg xmlns=""http://www.w3.org/2000/svg"" xml:space=""preserve"" width=""18px"" height=""18px"" version=""1.1"" style=""shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd""
+        viewBox=""0 0 2.98 2.98""
+         xmlns:xlink=""http://www.w3.org/1999/xlink""
+         xmlns:xodm=""http://www.corel.com/coreldraw/odm/2003"">
+         <defs>
+          <style type=""text/css"">
+           <![CDATA[
+            .str0 {stroke:white;stroke-width:0.31;stroke-miterlimit:22.9256}
+            .fil0 {fill:none}
+           ]]>
+          </style>
+         </defs>
+         <g id=""Layer_x0020_1"">
+          <metadata id=""CorelCorpID_0Corel-Layer""/>
+          <circle class=""fil0 str0"" cx=""1.49"" cy=""1.49"" r=""1.33""/>
+         </g>
+        </svg>";
+    }
+}
