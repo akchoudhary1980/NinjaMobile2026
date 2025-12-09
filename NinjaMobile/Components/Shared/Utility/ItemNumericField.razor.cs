a004bc2ae1a33c04b50018ca646aa5f06fb9b775
akchoudhary1980 <73695195+akchoudhary1980@users.noreply.github.com>
2025-12-09 11:10:25 +0530

    utility

---
+++ NinjaMobile/Components/Shared/Utility/ItemNumericField.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+#nullable disable
+namespace Ninja.Components.Shared.Utility
+{
+    public partial class ItemNumericField
+    {
+        [Parameter]
+        public double Value { get; set; } = 0;
+        [Parameter]
+        public double Step { get; set; } = 1;
+        [Parameter]
+        public double Min { get; set; } = 0;
+        [Parameter]
+        public double Max { get; set; } = 100;
+
+        [Parameter]
+        public EventCallback<double> ValueChanged { get; set; }
+
+
+        
+
+        private void OnValueChanged(ChangeEventArgs e)
+        {
+            if (double.TryParse(e.Value?.ToString(), out double newValue))
+            {
+                Value = newValue;
+                ValueChanged.InvokeAsync(Value);
+            }
+        }
+
+
+        private void Increment()
+        {
+            if (Value < Max)
+            {
+                Value = Value + Step;
+                ValueChanged.InvokeAsync(Value);
+            }
+                
+        }
+
+        private void Decrement()
+        {
+            if (Value > Min)
+            {
+                Value = Value - Step;
+                ValueChanged.InvokeAsync(Value);
+            }
+        }
+    }
+}
