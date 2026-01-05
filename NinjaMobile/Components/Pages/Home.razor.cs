9502d33917bf8a21d1a22c13af96f8d1aef4e040
akchoudhary1980 <73695195+akchoudhary1980@users.noreply.github.com>
2026-01-05 17:08:51 +0530

    pusehd

---
+++ NinjaMobile/Components/Pages/Home.razor.cs
@@ -0,0 +1,27 @@
+﻿using MudBlazor;
+using NinjaMobile.Data;
+#nullable disable
+
+namespace NinjaMobile.Components.Pages
+{
+    public partial class Home
+    {
+        List<SpeedDialItem> Actions;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            Actions = new()
+            {
+                new SpeedDialItem { Icon = Icons.Custom.Brands.WhatsApp, Color = Color.Success, Label = "WhatsApp", Action = () => OnClick("WhatsApp") },
+                new SpeedDialItem { Icon = Icons.Material.Filled.Phone, Color = Color.Primary, Label = "Phone", Action = () => OnClick("Phone") }
+            };
+        }
+
+        void OnClick(string name)
+        {
+            Console.WriteLine($"{name} clicked");
+        }
+
+    }
+}
---
+++ NinjaMobile/Data/SpeedDialItem.cs
@@ -0,0 +1,12 @@
+﻿using MudBlazor;
+#nullable disable
+namespace NinjaMobile.Data
+{
+    public class SpeedDialItem
+    {
+        public string Icon { get; set; }
+        public string Label { get; set; }
+        public Color Color { get; set; }
+        public Action Action { get; set; }
+    }
+}
